963743956c777f6e1a932144f572945d36f20083
Eric Boettner <boettner.eric@gmail.com>
2026-01-26 10:02:57 -0800

    fix: add hbomax

--- scripts/settings.js
+++ scripts/settings.js
@@ -24,6 +24,11 @@ let settings = {
     queries: ['input[aria-label*="search" i]', 'input[name*="search_query" i]'],
     enabled: true,
   },
+  hbo: {
+    url: "play.hbomax.com",
+    queries: ['a[aria-label*="Search" i]'],
+    enabled: true,
+  },
   netflix: {
     url: "www.netflix.com",
     queries: ['button[aria-label*="Search" i]', 'input[id*="searchInput" i]'],
